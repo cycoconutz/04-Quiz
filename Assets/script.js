245fa897d30a3e06b0e808c51858567b11876a17
John Yates <johndyates@gmail.com>
2022-04-05 20:45:07 -0500

    git change. got questions list to populate

--- Assets/script.js
+++ Assets/script.js
@@ -1,52 +1,82 @@
 var questionList = [{
   question:" Inside which HTML element do we put the JavaScript?",
-  answers:["<javascript>", "script", "<js>", "scripting"]},
+  answers:["<javascript>", "script", "<js>", "scripting"],
+  correct:"script"
+  },
   {
   question: "Is JavaScript case-sensitive?",
-  answers:["No", "Yes"]},
+  answers:["No", "Yes"],
+  correct:"Yes"
+  },
   {
   question:"3: Where is the correct place to insert a JavaScript?",
-  answers:["The <head> section", "Both the <head> section and the <body> section are correct", "The <body> section"]},
+  answers:["The <head> section", "Both the <head> section and the <body> section are correct", "The <body> section"],
+  correct:"Both the <head> section and the <body> section are correct"
+  },
   {
   question:"4:What is the correct syntax for referring to an external script called \"xxx.js\"?",
-  answers:["<script src=\"xxx.js\">", "<script name=\"xxx.js\"","<script href=\"xxx.js\""]},
+  answers:["<script src=\"xxx.js\">", "<script name=\"xxx.js\"","<script href=\"xxx.js\""],
+  correct:"<script src=\"xxx.js\">"
+  },
   {
   question:"5:The external JavaScript file must contain the <script> tag.",
-  answers:["True", "False"]}];
+  answers:["True", "False"],
+  correct:"True"
+  }];
   
 var timerElement = document.querySelector('#countdown');
 var scoreboard = document.querySelector('#scoreboard');
 var highscores = document.querySelector('#highscores');
 var quizQ = document.querySelector('#quiz-question');
-var quizA = Array.from(document.querySelectorAll('input[name=answer]'));
+var quizA = document.querySelector('#quizAnswer');
 var startButt = document.querySelector('#start-button');
 var submitButt = document.querySelector('#submit');
 var timerCount;
 var timer;
-var questionNum = 0;
-var answerNum = 0;
+
 startButt.addEventListener("click", startGame);
 
 function startGame(){
   timerCount = 120;
   startButt.setAttribute("class", "hidden")
   submitButt.setAttribute("class","show")
-  nextQuestion();
+  nextQuestion(0);
   startTimer();
 }
 
-function nextQuestion(){
-  quizQ.textContent = (questionNum+1) + '. ' + 
-  questionList[questionNum].question;
-//   var answers = questionList[questionNum].answers;
-//   quizA.forEach(function (input, i){
-//     quizA.setAttribute("class", "show")
-//     input.value = answers[i];
-//     input.checked = false;
-//     quizA.textContent = answers[i];
-// });
+function nextQuestion(questionNum){
+  var currentQuestion = questionList[questionNum];
+  console.log(questionNum, currentQuestion)
+  quizQ.textContent = (questionNum) + '. ' + currentQuestion.question;
+  var answersDOM = currentQuestion.answers.map((answer, answerIndex) => {
+  var label = document.createElement("label");
+  var input = document.createElement('input');
+  label.htmlFor = "q" + answerIndex;
+  label.textContent = answer; 
+  input.id = "q" + answerIndex;
+  input.type = "radio";
+  input.name = "answer";
+  input.value = answer;
+  input.className = "answers";
+  quizA.appendChild(label);
+  quizA.appendChild(input);
+  });
+console.log(quizA)
+  quizA.onsubmit = (e) => {
+    console.log(questionNum);
+  e.preventDefault();
+    checkAnswer(currentQuestion, questionNum)};
 }
 
+function checkAnswer(currentQuestion, questionNum){
+console.log("wat");
+  if ($('input.answers:checked').val() == currentQuestion.correct){
+    nextQuestion(questionNum+1);
+    } else{
+        timerCount-10;
+        nextQuestion();
+    }
+}
 
 function startTimer() {
   // Sets timer
@@ -81,4 +111,9 @@ function finished(){
   quizQ.textContent = "You finished with time remaining! Great Job!"
   startButt.disabled = false;
   highscores();
+}
+
+function highscores(){
+ var initials = prompt("Enter your initials");
+ initials.textContent=(initials + "--" + timerCount);
 }
